31832f82eb560d5f8279b2fef108620a13d1e5fa
lahirusb97 <lahirushirandev@gmail.com>
2025-02-23 11:24:44 +0530

    url

--- myapi/settings.py
+++ myapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kcu5_435$20fw^btdzjpe%=h+6ztvlgcq&vk8ddq1ifn7_^le_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['visionmain-a9804.web.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['visionmain-a9804.web.app', '3.1.203.44', 'localhost']
 
 # Application definition
 
